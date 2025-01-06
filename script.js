caba06efdc5cd93e88fa69f61b659a8627cc7c27
maridgarcia <maridgarcia045@gmail.com>
2025-01-06 10:54:54 -0300

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+const button = document.querySelector(".main-bt");
+
+button.addEventListener("click", async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            let text = Array.from(document.querySelectorAll('.file-info a.Link--primary'))
+                            .map(node => node.textContent.trim())
+                            .join("\n");
+            return text;
+        }
+    }).then((injectionResults) => {
+        const result = injectionResults[0]?.result;
+
+        if (result) {
+            navigator.clipboard.writeText(result).then(() => {
+                alert("Nomes dos arquivos copiados com sucesso!");
+            }).catch(err => {
+                alert("Erro ao copiar para área de transferência.");
+            });
+        }
+    }).catch(err => {
+        console.error("Erro ao injetar script:", err);
+    });
+});
+
